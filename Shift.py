768fc94a0b051d0be349a4355e07103e5d8a9f6d
Wente Li <wenteli_utaustin@utexas.edu>
2020-07-01 18:27:17 -0500

    Change line 21 from Data[0:44] to Data[0:43]. Probably, if z coordinate is large, then 0:44 is too large and ruin z coordinate.

--- Shift.py
+++ Shift.py
@@ -5,20 +5,20 @@
 # Purpose: To make the slab in the center of the simulation cell
 
 
-ShiftDis = +13.5663      # Indicate the shift distance
+ShiftDis = 20      # Indicate the shift distance
 
 import numpy
 
-File = open("CONTCAR.vasp")      # THe header should be deleted
+File = open("QW.vasp")      # THe header should be deleted
 Data = File.readlines()
-data=numpy.loadtxt('CONTCAR.vasp')
+data=numpy.loadtxt('QW.vasp')
 
 Export = open("Shift.txt",'w')
 
 
 
 for i in range (0,len(data)):
-    Export.write(Data[i][0:44]);
+    Export.write(Data[i][0:43]);
     Export.write('%.8f'%(data[i,2]+ShiftDis));
     Export.write('\r\n')
 
